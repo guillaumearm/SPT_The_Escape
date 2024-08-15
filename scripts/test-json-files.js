6569cc4d7147de0d2b655bbe0f393665d938ea70
Guillaume ARM <trapcodien@gmail.com>
2024-08-15 02:22:08 +0200

    ci: improve test-json-files adding json5 and jsonc parsers (#17)
    * ci: improve test-json-files adding json5 and jsonc parsers
    
    * ci: use of npm cache and matrix in gh workflow
    
    * ci: simplify the workflow
    
    * ci(fix): remove useless matrix
    
    ---------
    
    Co-authored-by: Trap <contact@trapcodien.com>

--- scripts/test-json-files.js
+++ scripts/test-json-files.js
@@ -1,7 +1,19 @@
 const fs = require("node:fs/promises");
 const { glob } = require("glob");
+const jsonc = require("jsonc");
+const JSON5 = require("json5");
 
 const IS_VERBOSE = true;
+
+const JSON_PARSERS = {
+  json: JSON.parse.bind(JSON),
+  json5: JSON5.parse.bind(JSON5),
+  jsonc: jsonc.parse.bind(jsonc),
+};
+
+const getParser = (parserName) => {
+  return JSON_PARSERS[parserName];
+};
 
 const logVerbose = (...args) => {
   if (IS_VERBOSE) {
@@ -9,29 +21,51 @@ const logVerbose = (...args) => {
   }
 };
 
-const parseJsonFile = async (filePath) => {
-  const content = await fs.readFile(filePath);
+const parseFile = async (parser, filePath) => {
+  const content = await fs.readFile(filePath, "utf-8");
 
   try {
-    return JSON.parse(content);
+    return parser(content);
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
 
-const main = async () => {
-  const jsonFilePaths = await glob("**/*.json");
+const getFileTypeFromArgv = (argv) => {
+  const [_arg1, _arg2, rawFileType] = argv;
+  const fileType = rawFileType?.toLowerCase().trim();
+  const parserFound = Boolean(JSON_PARSERS[fileType]);
 
-  for (const filePath of jsonFilePaths) {
-    logVerbose(`> parsing of ${filePath}`);
-    const _parsed = await parseJsonFile(filePath);
+  if (!parserFound && fileType) {
+    console.error(
+      `> Bad filetype argument providen: "${fileType}". use json, json5 or jsonc`
+    );
+    process.exit(1);
+  } else if (!parserFound && !fileType) {
+    console.error("> No filetype argument providen. use json, json5 or jsonc");
+    process.exit(1);
   }
 
-  console.log(`> Successfully parsed ${jsonFilePaths.length} json files.`);
+  return fileType;
 };
 
-main()
+const main = async (argv) => {
+  const fileType = getFileTypeFromArgv(argv);
+  const parser = getParser(fileType);
+  const filePaths = await glob(`**/*.${fileType}`);
+
+  console.log(`> Use '${fileType}' parser on ${filePaths.length} file.`);
+
+  for (const filePath of filePaths) {
+    logVerbose(`> parsing of ${filePath}`);
+    const _parsed = await parseFile(parser, filePath);
+  }
+
+  console.log(`> Successfully parsed ${filePaths.length} ${fileType} files.`);
+};
+
+main(process.argv)
   .then((res) => {
     if (res) {
       console.log(res);
