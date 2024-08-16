3dbe6fee2f4ba8536bf05d71f4e7fcadab246e5d
Trap <contact@trapcodien.com>
2024-08-16 10:58:28 +0200

    fix: improve sync.sh script

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rsync -avz --update --existing ../../user .
-rsync -avz --update --existing ../../BepInEx .
+rsync -avz --delete --update --existing ../../user .
+rsync -avz --delete --update --existing ../../BepInEx .
 
-rsync -avz --include="package.json" --include="*/" --exclude='*' ../../user/mods ./user/
+rsync -avz --delete --include="package.json" --include="*/" --exclude='*' ../../user/mods ./user/
